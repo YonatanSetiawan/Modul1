fd72a651bba3fb504efc32a8ed44fbac4ca966a3
YonatanSetiawan <114370654+YonatanSetiawan@users.noreply.github.com>
2022-09-26 12:22:25 +0700

    Modul 1 Yonatan Setiawan 212410101102

---
+++ Modul 1/Modul 1/Laptop.cs
@@ -0,0 +1,89 @@
+﻿using ProcessorKak;
+class Laptop
+{
+    public string merk;
+    public string tipe;
+    public Vga vga;
+    public Processor processor;
+
+
+    public void LaptopDinyalakan()
+    {
+        Console.WriteLine($"Laptop {merk} {tipe} menyala" );
+    }
+    public void LaptopDimatikan()
+    {
+        Console.WriteLine($"Laptop {merk} {tipe} mati");
+    }
+}
+class ASUS : Laptop
+{
+    public ASUS()
+    {
+        merk = "ASUS";
+    }
+}
+class ACER : Laptop
+{
+    public ACER()
+    {
+        merk = "ACER";
+    }
+}
+class Lenovo : Laptop
+{
+    public Lenovo()
+    {
+        merk = "Lenovo";
+    }
+}
+class ROG : ASUS
+{
+    public ROG()
+    {
+        tipe = "ROG";
+    }
+}
+class Vivobook : ASUS
+{
+    public Vivobook()
+    {
+        tipe = "Vivobook";
+    }
+    public void Ngoding()
+    {
+        Console.WriteLine("Ctak Ctak Ctak, error lagi!!");
+    }
+}
+class Swift : ACER
+{
+    public Swift()
+    {
+        tipe = "Swift";
+    }
+}
+class Predator : ACER
+{
+    public Predator()
+    {
+        tipe = "Predator";
+    }
+    public void BermainGame()
+    {
+        Console.WriteLine($"Laptop {merk} {tipe} sedang memainkan game");
+    }
+}
+class Idepad : Lenovo
+{
+    public Idepad()
+    {
+        tipe = "Idepad";
+    }
+}
+class Legion : Lenovo
+{
+    public Legion()
+    {
+        tipe = "Legion";
+    }
+}
---
+++ Modul 1/Modul 1/Processor.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection.Metadata;
+namespace ProcessorKak
+{
+    class Processor
+    {
+        public string merk;
+        public string tipe;
+    }
+    class Intel : Processor
+    {
+        public Intel()
+        {
+            merk = "Intel";
+        }
+
+    }
+    class AMD : Processor
+    {
+        public AMD()
+        {
+            merk = "AMD";
+        }
+    }
+    class CoreI3 : Intel
+    {
+        public CoreI3()
+        {
+            tipe = "Corei3";
+        }
+    }
+    class CoreI5 : Intel
+    {
+        public CoreI5()
+        {
+            tipe = "Corei5";
+        }
+    }
+    class CoreI7 : Intel
+    {
+        public CoreI7()
+        {
+            tipe = "Corei7";
+        }
+    }
+    class Ryzen : AMD
+    {
+        public Ryzen()
+        {
+            tipe = "RAYZEN";
+        }
+    }
+    class Athlon : AMD
+    {
+        public Athlon()
+        {
+            tipe = "ATHLON";
+        }
+    }
+}
