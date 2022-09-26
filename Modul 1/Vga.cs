4460042e6f66ac4d72b18e0e5a51fa093b9a0f52
YonatanSetiawan <114370654+YonatanSetiawan@users.noreply.github.com>
2022-09-26 12:19:11 +0700

    Modul 1 Inheritence Yonatan Setiawan 212410101102

---
+++ Modul 1/Vga.cs
@@ -0,0 +1,19 @@
+﻿class Vga
+{
+    public string merk;
+
+}
+class Nvidia : Vga
+{
+    public Nvidia()
+    {
+        merk = "Nvdia";
+    }
+}
+class AMD : Vga
+{
+    public AMD()
+    {
+        merk = "AMD";
+    }
+}
